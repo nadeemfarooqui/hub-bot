7597648a67f627387c47588652ee7322f14af2d2
kvetrivillalan <kvetrivillalan@ebay.com>
2016-11-17 17:29:54 -0800

    read news from json

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var builder = require('botbuilder');
 var restify = require('restify');
 
+var news = require('./news');
+
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -17,7 +20,6 @@ server.post('/api/messages', connector.listen());
 
 // Bot dialog
 bot.dialog('/', [
-
     function (session) {
             session.send('Welcome to the Hub bot!');
             builder.Prompts.text(session, 'Reply to hear the latest news...');
@@ -25,57 +27,27 @@ bot.dialog('/', [
 
     function (session, results) {
         var entered_reply = results.response.entity;
-        var cards = getCardsAttachments();    
-        // create reply with Carousel AttachmentLayout
-        var latest_news = new builder.Message(session)
-            .attachmentLayout(builder.AttachmentLayout.carousel)
-            .attachments(cards);
-        session.send(latest_news);
+        getTopNews(session, function(result){    
+            var latest_news = new builder.Message(session)
+                .attachmentLayout(builder.AttachmentLayout.carousel)
+                .attachments(result);
+            session.send(latest_news);
+        });
     }
 ]);
 
-function getCardsAttachments(session) {
-    return [
-        new builder.HeroCard(session)
-            .title("2016's Spooktacular Costumes and Decorations")
-            .text("From Japan to San Jose HQ, heres our global Halloween spirit caught on camera.")
+function getTopNews(session, callback) {
+    news.fetch_top_news(page_index=0, function (result) {
+        var hero_cards = []
+        result.forEach(function(news){
+           hero_cards.push(new builder.HeroCard(session)
+            .title(news.title)
+            .text(news.subtitile)
             .images([
-                builder.CardImage.create(session, 'https://lh4.googleusercontent.com/f0nGtJFK2ZA74paeZ3fm0Q3XjruMqtsyhyFJ59X2uBs4hdihb35SeRV41UF_VYsTlzX9CNIWTvXSPug=w2880-h1468')
+                builder.CardImage.create(session, news.image_url)
             ])
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://hub.corp.ebay.com/news/10292/', 'More')
-            ]),
-
-        new builder.ThumbnailCard(session)
-            .title("eBay Rises to the \"Bake Off\" Challenge")
-            .subtitle('Blazing fast, planet-scale NoSQL')
-            .text('Sales jump on eBay in conjunction with the popular BBC show.')
-            .images([
-                builder.CardImage.create(session, 'https://lh3.googleusercontent.com/zpMaSXShmOaEoumjnOk2LNCDgHuUBF2RA-NXeNfwgEu2dRrXDA_V_eiH--8xmSaRFGtQBo7-OdPQYl0=w2880-h1468')
-            ])
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://hub.corp.ebay.com/news/10295/', 'More')
-            ]),
-
-        new builder.HeroCard(session)
-            .title('U.S. Benefits Open Enrollment')
-            .text('Open Enrollment (Nov. 1-11) is the time to make sure your benefits coverage keeps up with your needs.')
-            .images([
-                builder.CardImage.create(session, 'https://lh4.googleusercontent.com/Y78bwHbiqGL8M24YQ8OZVF3DoMifTsX2WZXvvPh89WjLGPTYPkb88vyQ1GYMnkLQMYGdgx35ku1suyE=w2880-h1468')
-            ])
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://hub.corp.ebay.com/news/10283/', 'More')
-            ]),
-
-        new builder.ThumbnailCard(session)
-            .title('Making everyone a WIN-ner')
-            .subtitle('Build powerful intelligence into your applications to enable natural and contextual interactions')
-            .text('Enable natural and contextual interaction with tools that augment users\' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.')
-            .images([
-                builder.CardImage.create(session, 'https://lh4.googleusercontent.com/0_xFAozSGi9iMam_2K4JH-tNeSwOzoZsOT1kgYiHtZQhJMLsAKXMKKKVJsqnn-6EsXrIcpWrNynqA3E=w2880-h1468')
-            ])
-            .buttons([
-                builder.CardAction.openUrl(session, 'https://hub.corp.ebay.com/news/10279/ ', 'More')
-            ])
-    ];
+           );        
+        });
+        callback(hero_cards);
+    });
 }
---
+++ news.js
@@ -0,0 +1,22 @@
+module.exports = {
+    fetch_top_news: function (page_index=0, callback) {
+        var fs = require('fs');
+        var path = require('path');
+        var filePath = path.join(__dirname, 'json_data/topnews.json');    
+        fs.readFile(filePath, 'utf8', function (err, data) {   
+            var top_news = []    
+            var news;          
+            news = JSON.parse(data).data;     
+            news.forEach(function(news){        
+                var news_item = { };
+                news_item["title"] = news.title;
+                news_item["subtitile"] = news.subTitle;
+                news_item["image_url"] = news.absimageUrl.replace('https://hubpub.corp.ebay.com','https://hub.corp.ebay.com');
+                top_news.push(news_item);
+            });
+            if (page_index > 3) {page_index = 3;}
+            var result = top_news.slice((4*page_index),(4*page_index)+4);
+            callback(result);
+        });            
+    }
+};
