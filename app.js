7cc5dafd4477add13ca1ff35aa63407c63090423
kvetrivillalan <kvetrivillalan@ebay.com>
2016-11-18 11:25:11 -0800

    revert

--- app.js
+++ app.js
@@ -16,64 +16,24 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+// Bot dialog
 bot.dialog('/', [
     function (session) {
-        session.send("Hello... I'm hubx bot.");
-        session.beginDialog('/menu');
+            session.send('Welcome to the Hub bot!');
+            builder.Prompts.text(session, 'Reply to hear the latest news...');
     },
+
     function (session, results) {
-        session.endConversation("Goodbye until next time...");
-    }
-]);
-
-bot.dialog('/menu', [
-    function (session) {
-        builder.Prompts.choice(session, "What are you looking for?", ["News","Benefits","People-Search","quit"]);
-    },
-    function (session, results) {
-       if (results.response && results.response.entity != '(quit)') {
-            session.beginDialog('/' + results.response.entity);
-        } else {
-            session.send("Good bye!");
-            session.endDialog();
-        }
-    },
-    function (session) {
-        // Reload menu
-        session.replaceDialog('/menu');
-    }
-]).reloadAction('showMenu', null, { matches: /^(menu|back)/i });
-
-
-bot.dialog('/News', [
-    function (session) {
-        session.send("Here are the top news from ebay...");
-        session.sendTyping();
+        var entered_reply = results.response.entity;
         getTopNews(session, function(result){    
             var latest_news = new builder.Message(session)
                 .attachmentLayout(builder.AttachmentLayout.carousel)
                 .attachments(result);
-            session.endDialog(latest_news);
+            session.send(latest_news);
         });
-    }
-]);
-
-bot.dialog('/Benefits', [
-    function (session) {
-        session.send("Benefits under construction...");
-        session.endDialog();
-    }
-]);
-
-
-bot.dialog('/People-Search', [
-    function (session) {
-        session.send("People-Search under construction...");
-        session.endDialog();
     }
 ]);
 
