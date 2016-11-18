d6ab1373ee29efb5db5b6b0842cfa1b21b9bfbfa
kvetrivillalan <kvetrivillalan@ebay.com>
2016-11-18 11:36:04 -0800

    get it back

--- app.js
+++ app.js
@@ -16,24 +16,85 @@ var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-// Bot dialog
+
 bot.dialog('/', [
     function (session) {
-            session.send('Welcome to the Hub bot!');
-            builder.Prompts.text(session, 'Reply to hear the latest news...');
+        // Send a greeting and show help.
+        var card = new builder.HeroCard(session)
+            .title("Hubx Bot")
+            .text("Your bot to get information from Hubx.")
+            .images([
+                 builder.CardImage.create(session, "http://docs.botframework.com/images/demo_bot_image.png")
+            ]);
+        var msg = new builder.Message(session).attachments([card]);
+        session.send(msg);
+        session.send("Hi... I'm the Hubx bot.");
+        session.beginDialog('/help');
     },
-
     function (session, results) {
-        var entered_reply = results.response.entity;
+        // Display menu
+        session.beginDialog('/menu');
+    },
+    function (session, results) {
+        // Always say goodbye
+        session.send("Ok... See you later!");
+    }
+]);
+
+bot.dialog('/help', [
+    function (session) {
+        session.endDialog("Global commands that are available anytime:\n\n* menu - Exits a demo and returns to the menu.\n* goodbye - End this conversation.\n* help - Displays these commands.");
+    }
+]);
+
+
+bot.dialog('/menu', [
+    function (session) {
+        builder.Prompts.choice(session, "What are you looking for?", ["News","Benefits","People-Search","quit"]);
+    },
+    function (session, results) {
+       if (results.response && results.response.entity != '(quit)') {
+            session.beginDialog('/' + results.response.entity);
+        } else {
+            session.endDialog();
+        }
+    },
+    function (session) {
+        // Reload menu
+        session.replaceDialog('/menu');
+    }
+]).reloadAction('reloadMenu', null, { matches: /^(menu|show menu)/i });
+
+
+bot.dialog('/News', [
+    function (session) {
+        session.send("Here are the top news from ebay...");
+        session.sendTyping();
         getTopNews(session, function(result){    
             var latest_news = new builder.Message(session)
                 .attachmentLayout(builder.AttachmentLayout.carousel)
                 .attachments(result);
-            session.send(latest_news);
+            session.endDialog(latest_news);
         });
+    }
+]);
+
+bot.dialog('/Benefits', [
+    function (session) {
+        session.send("Benefits under construction...");
+        session.endDialog();
+    }
+]);
+
+
+bot.dialog('/People-Search', [
+    function (session) {
+        session.send("People-Search under construction...");
+        session.endDialog();
     }
 ]);
 
--- news.js
+++ news.js
@@ -1,12 +1,27 @@
 'use strict';
+let fs = require('fs');
+
 function fetch_top_news(page_index, callback) {
-    result = [];
-    return result;
+     fs.readFile('./json_data/topnews.json', 'utf8', function (err, data) {   
+         var top_news = [];    
+         var news_items;          
+         news_items = JSON.parse(data).data;     
+         news_items.forEach(function(each_news){        
+             var news_item = {};
+             news_item["title"] = each_news.title;
+             news_item["subtitile"] = each_news.subTitle;
+             news_item["image_url"] = each_news.absimageUrl.replace('https://hubpub.corp.ebay.com','https://hub.corp.ebay.com');
+             top_news.push(news_item);
+         });
+         if (page_index > 3) {page_index = 3;}
+         var result = top_news.slice((4*page_index),(4*page_index)+4);
+         callback(result);
+     });            
 }
+
 
 let news = {
     fetch_top_news:fetch_top_news
 };
 
 module.exports = news;
-
